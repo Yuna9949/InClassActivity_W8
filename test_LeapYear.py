8ed47d77f5ee31feb33d7d80c8b0081e94d1b8dc
Yuna Oh <ustina0409@gmail.com>
2021-05-22 21:00:25 -0700

    test leap year

---
+++ test_LeapYear.py
@@ -0,0 +1,14 @@
+import unittest
+import LeapYear
+class TestLeapYear(unittest.TestCase):
+    def test_LeapYear_1(self):
+        self.assertEqual(LeapYear.leapyear(2012), True)
+
+    def test_LeapYear_2(self):
+        self.assertEqual(LeapYear.leapyear(2100), False)
+
+    def test_LeapYear_3(self):
+        self.assertEqual(LeapYear.leapyear(2000), True)
+    
+#if __name__=='__main__':
+#    unittest.main()
