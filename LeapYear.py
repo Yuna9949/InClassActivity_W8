005d21a64a7cdec2274a5f16d8b9f129f783ffdb
Yuna Oh <ustina0409@gmail.com>
2021-05-22 20:51:09 -0700

    Leap Year

---
+++ LeapYear.py
@@ -0,0 +1,17 @@
+def leapyear(y):
+    LYChk = False
+    if y % 4 == 0:
+        if y % 100 == 0:
+            if y % 400 == 0:
+                LYChk = True
+        else:
+            LYChk = True
+    return LYChk
+
+y = int(input('Input year : '))
+if leapyear(y):
+    print(y, end='')
+    print(' is a leap year.')
+else :
+    print(y, end='')
+    print(' is not a leap year.')
